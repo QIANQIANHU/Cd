aa80e8a8d0f0ff1d31e788b90f5ab4c3508ac58a
Ainur Dzhaianbaeva and Qianqian Hu <ainur@>
2018-02-06 11:15:35 -0800

    Add new instance with Add/Remove method

--- Cd.cs
+++ Cd.cs
@@ -7,7 +7,7 @@ class Cd
   public string Artist;
   public int Price;
 
-  public Cd(string albumTitle, string albumArtist, int albumPrice)
+  public Cd(string albumTitle, string albumArtist, int albumPrice = 10)
   {
     Title = albumTitle;
     Artist = albumArtist;
@@ -19,23 +19,29 @@ public class Program
 {
   public static void Main()
   {
-    Cd firstCd = new Cd();
-    firstCd.Title = "Master of Reality";
-    firstCd.Artist = "Black Sabbath";
-    firstCd.Price = 10;
-
-    Cd secondCd = new Cd();
-    secondCd.Title = "Electric Ladyland";
-    secondCd.Artist = "Jimi Hendrix";
-    secondCd.Price = 10;
-
-    Cd thirdCd = new Cd();
-    thirdCd.Title = "Nevermind";
-    thirdCd.Artist = "Nirvana";
-    thirdCd.Price = 10;
+    Cd firstCd = new Cd("Master of Reality", "Black Sabbath");
+    Cd secondCd = new Cd("Electric Ladyland", "Jimi Hendrix");
+    Cd thirdCd = new Cd("Nevermind", "Nirvana");
+    // Cd firstCd = new Cd();
+    // firstCd.Title = "Master of Reality";
+    // firstCd.Artist = "Black Sabbath";
+    // firstCd.Price = 10;
+    //
+    // Cd secondCd = new Cd();
+    // secondCd.Title = "Electric Ladyland";
+    // secondCd.Artist = "Jimi Hendrix";
+    // secondCd.Price = 10;
+    //
+    // Cd thirdCd = new Cd();
+    // thirdCd.Title = "Nevermind";
+    // thirdCd.Artist = "Nirvana";
+    // thirdCd.Price = 10;
 
     List<Cd> Cds = new List<Cd>() { firstCd, secondCd, thirdCd };
 
+    Cd fourthCd = new Cd("I Don’t Get It", "Pork Lion", 50);
+    Cds.Add(fourthCd);
+    
     foreach (Cd album in Cds)
     {
       Console.WriteLine(album.Title);
